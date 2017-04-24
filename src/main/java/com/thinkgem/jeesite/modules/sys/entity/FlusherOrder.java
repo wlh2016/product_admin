bd54c25eda598b000210c12fa986828148d90db1
wlh2016 <wlhhjh77@163.com>
2017-04-24 18:16:21 +0800

    4-24新增FlusherOrder实体类

---
+++ src/main/java/com/thinkgem/jeesite/modules/sys/entity/FlusherOrder.java
@@ -0,0 +1,15 @@
+package com.thinkgem.jeesite.modules.sys.entity;
+
+/**
+ * @author Tony_Wu
+ * @date 2017-4-24
+ */
+public class FlusherOrder {
+
+    private long id;
+    private String customerName;
+    private String phone;
+    private String address;
+
+
+}
