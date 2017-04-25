4f6f0021d8afa839d400a9e327d3036c286fcc3a
wlh2016 <wlhhjh77@163.com>
2017-04-25 15:40:01 +0800

    优化订单业务

---
+++ src/main/java/com/thinkgem/jeesite/modules/sys/dao/CustomerDao.java
@@ -0,0 +1,16 @@
+package com.thinkgem.jeesite.modules.sys.dao;
+
+import com.thinkgem.jeesite.common.persistence.CrudDao;
+import com.thinkgem.jeesite.common.persistence.annotation.MyBatisDao;
+import com.thinkgem.jeesite.modules.sys.entity.Customer;
+
+/**
+ * @author Tony_Wu
+ * @date 2017-4-25
+ */
+@MyBatisDao
+public interface CustomerDao extends CrudDao<Customer> {
+
+
+
+}
